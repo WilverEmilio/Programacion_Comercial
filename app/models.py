f6fba8c0e75eb16f70fa753f457c1f349c9aedca
Arturo Monterroso <110266239+arturomonterroso25@users.noreply.github.com>
2024-07-19 12:02:14 -0600

    Delete app directory

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from .conexion import Base
-
-class Registr(Base):
-    __tablename__ = "ingreso"
-    idingreso = Column(Integer, primary_key = True, index = True)
-    usuario = Column(String(255))
-    contrasena = Column(String(500))
-    correo_electronico = Column(String(255))
